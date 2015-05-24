a2c348b81991e6c10467cc349c44d53bc2d4cf72
sumeet rohatgi <sum.rohatgi@gmail.com>
2015-05-24 12:17:48 -0700

    mapping to api inputs

--- api_wrapper.py
+++ api_wrapper.py
@@ -18,11 +18,33 @@ class ReminderAPI:
             raise Exception('unable to parse apiconn: {}'.format(apiconn))
 
     def make_call(self, row):
-        data = json.dumps(row)
-        r = requests.post(self.uri, data=data, headers=self.auth)
-        logging.debug(r)
+        payload = {
+            'email_id': row['email_id'],
+            'brand_id': row['brand_id'],
+            'last_transaction_date': row['last_transaction_date'].strftime('%Y/%m/%d'),
+            'first_name': row['first_name'],
+            'last_name': row['last_name'],
+            'contacts': [
+                {
+                    'first_name': row['contact_first_name'],
+                    'last_name': row['contact_last_name']
+                }
+            ],
+            'occasion_date': row['occasion_date'].strftime('%Y/%m/%d'),
+            'gift_message': row['gift_message'],
+            'occasion_type': 'MOTHERS_DAY|ANNIVERSARY|HALLOWEEN|CHRISTMAS|BIRTHDAY'
+        }
 
-payload = {
+        for ot in ['mothersday', 'bday', 'xmas', 'anniversary', 'halloween']:
+            if row[ot] == '1':
+                payload['occasion_type'] = ot
+                break
+
+        data = json.dumps(payload)
+        # r = requests.post(self.uri, data=data, headers=self.auth)
+        # logging.debug(r)
+
+payload1 = {
     'email_id': '',
     'brand_id': 4,  # for CHERYL: 4, FM: 5, TPF: 6
     'last_transaction_date': '',
@@ -39,7 +61,7 @@ payload = {
     'occasion_type': 'MOTHERS_DAY|ANNIVERSARY|HALLOWEEN|CHRISTMAS|BIRTHDAY'
 }
 
-response = {
+response1 = {
     'reminder_id': 1000,
     'user_id': 2000,
     'contact_id': 3000
